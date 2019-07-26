93bdb311bebb05aed9b3416e2e29e6a7d2c36afb
daniel devadasan <dani.raja@yahoo.com>
2019-07-25 20:35:35 -0400

    Delete crawler.sh

--- src/crawler.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-HOME="https://www.yahoo.com"
-DOMAINS="yahoo.com"
-DEPTH=3
-OUTPUT="./output/urls.csv"
-
-wget -r --spider --delete-after --force-html -D "$DOMAINS" -l $DEPTH "$HOME" 2>&1 \
-    | grep '^--' | awk '{ print $3 }' | grep -v '\. \(!css\|images\|js\|png\|gif\|jpg\)$' | sort | uniq > $OUTPUT
