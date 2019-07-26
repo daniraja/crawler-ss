4753e8389ecfb354df32b4fd0fb957d75497dccf
daniel devadasan <dani.raja@yahoo.com>
2019-07-26 11:28:18 -0400

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,4 +1,12 @@
+const waitTime = 50000;
 exports.config = {
   seleniumAddress: 'http://localhost:4444/wd/hub',
-  specs: ['screenshot-spec.js', 'readfile.js']
+  specs: ['screenshot-spec.js'],
+
+  // Options to be passed to Jasmine-node.
+  jasmineNodeOpts: {
+    showColors: true, // Use colors in the command line report.
+    defaultTimeoutInterval: waitTime,
+    allScriptsTimeout: waitTime
+  }
 };
